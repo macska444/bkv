e7f74d77ab5631e69b1b2ab4f39610ac28429d6e
Macska <judit.akos@idomsoft.hu>
2020-09-24 10:13:11 +0200

    validateTicketNumber: null

--- bkv_validator/src/main/java/hu/macska/bkv/ticket/TicketFelismeres.java
+++ bkv_validator/src/main/java/hu/macska/bkv/ticket/TicketFelismeres.java
@@ -2,6 +2,8 @@ package hu.macska.bkv.ticket;
 
 public class TicketFelismeres {
     public void validateTicketNumber(String ticketNumber) {
+        if (ticketNumber == null)
+            throw new TicketNumberIsNull();
 
     }
 }
